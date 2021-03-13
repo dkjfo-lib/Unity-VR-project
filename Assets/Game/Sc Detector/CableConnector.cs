1bbb8b84b6ef7b9003ea945b6500f6e1ca6a2150
Mike <mihailfd@gmail.com>
2021-03-13 21:36:05 +0300

    finished 2 main scenes

---
+++ Assets/Game/Sc Detector/CableConnector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class CableConnector : MonoBehaviour
+{
+    public Transform obj1;
+    public Transform obj2;
+    LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 diff = obj1.position - obj2.position;
+        Vector3 midPoint = diff / 2 + obj2.position - Vector3.up * .2f;
+        Vector3 mid1Point = Vector3.Scale((obj1.position - midPoint), new Vector3(1, .5f, 1)) + midPoint;
+        Vector3 mid2Point = Vector3.Scale((obj2.position - midPoint), new Vector3(1, .5f, 1)) + midPoint;
+        lineRenderer.SetPositions(new[]
+        {
+            obj1.position,
+            mid1Point,
+            midPoint,
+            mid2Point,
+            obj2.position,
+        });
+    }
+}
---
+++ Assets/Game/Sc Detector/Detector.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HTC.UnityPlugin.Vive;
+using HTC.UnityPlugin.ColliderEvent;
+
+public class Detector : BasicGrabbable
+{
+    public Transform probeEnd;
+    public Transform gasPoint;
+    public Text detectorDisplay;
+    public float detectorValue = 0;
+    public bool turnedOn = false;
+    public ColliderButtonEventData.InputButton targetButton = ColliderButtonEventData.InputButton.Trigger;
+
+
+    public float time2Switch = 3f;
+    public float timeHeld = 0;
+
+    private void Start()
+    {
+        detectorDisplay.gameObject.SetActive(turnedOn);
+    }
+
+    private void Update()
+    {
+        if (turnedOn)
+        {
+            Vector3 diff = probeEnd.position - gasPoint.position;
+            detectorValue = diff.sqrMagnitude;
+            detectorDisplay.text = $"{detectorValue:f3}";
+        }
+    }
+
+    public override void OnColliderEventDragFixedUpdate(ColliderButtonEventData eventData)
+    {
+        if (isGrabbed && eventData.button == targetButton)
+        {
+            CountTime2Toggle();
+        }
+        base.OnColliderEventDragFixedUpdate(eventData);
+    }
+
+    public override void OnColliderEventDragUpdate(ColliderButtonEventData eventData)
+    {
+        if (isGrabbed && eventData.button == targetButton)
+        {
+            CountTime2Toggle();
+        }
+        base.OnColliderEventDragUpdate(eventData);
+    }
+
+    public override void OnColliderEventDragEnd(ColliderButtonEventData eventData)
+    {
+        if (eventData.button == targetButton)
+        {
+            timeHeld = 0;
+        }
+        base.OnColliderEventDragEnd(eventData);
+    }
+
+    void CountTime2Toggle()
+    {
+        timeHeld += Time.deltaTime;
+        if (timeHeld > time2Switch)
+        {
+            ToggleDetector();
+            timeHeld = 0;
+        }
+    }
+
+    void ToggleDetector()
+    {
+        turnedOn = !turnedOn;
+        detectorDisplay.gameObject.SetActive(turnedOn);
+    }
+}
